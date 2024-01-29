9b3bde87f415742dec6782af363213d1185c55cc
FranklinJaya2006 <148520978+FranklinJaya2006@users.noreply.github.com>
2024-01-30 00:29:07 +0800

    Add files via upload

---
+++ faktorial.py
@@ -0,0 +1,19 @@
+print("pilih salah satunya")
+print("1. Penjumblahan")
+print("2. Perkalian")
+angka = input("Pilih Angka :")
+
+if angka == "1":
+  print("Penjumblahan")
+  Angka1 = int(input("Angka :"))
+  Angka1 = (1, Angka1)
+  for i in Angka1 :
+    print(Angka1)
+    
+else: 
+    angka == "2" 
+    print("Perkalian")
+    Angka1 = int(input("Angka :"))
+    for i in range(1, Angka1):
+        Angka1 *= i
+        print(Angka1)
